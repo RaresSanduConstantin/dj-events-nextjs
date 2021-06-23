41320f648c9e8477d0185f7ea5ab5c3febd4c5fb
Sandu <sandurares57@gmail.com>
2021-06-23 13:54:01 +0300

    Add pagination, layout component

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import styles from '../styles/Layout.module.css'
+
+export default function Layout({title, keywords, description, children}) {
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name='description' content={description} />
+                <meta name='keywords' content={keywords} />
+            </Head>
+            <div className={styles.container}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+Layout.defaultProps = {
+    title: 'DJ Events | Find the hottest partiests',
+    description: 'Find the latest DJ and other muscical events',
+    keywords: 'music, dj, edm, events',
+}
