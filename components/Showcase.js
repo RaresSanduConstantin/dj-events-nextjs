c260962b1380c7aca09758e85431e072d7acbd4b
Sandu <sandurares57@gmail.com>
2021-07-13 11:06:40 +0300

    13.07.2021

---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+
+import styles from "@/styles/Shouwcase.module.css"
+
+export default function Showcase() {
+    return (
+        <div className={styles.showcase}>
+            <h1>Welcome to the party</h1>
+            <h2>Find the best dj events</h2>
+        </div>
+    )
+}
